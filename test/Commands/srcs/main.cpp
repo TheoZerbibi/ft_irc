1470ff504979ae125b4ce92d77c2b66da0e07860
Theo ZERIBI <tzeribi@gmail.com>
2023-05-30 03:08:26 +0200

    Add dock to README

--- test/Commands/srcs/main.cpp
+++ test/Commands/srcs/main.cpp
@@ -15,15 +15,12 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-
 	std::map<std::string, Command*> commandList;
 	commandList.insert(std::make_pair("test", new TestCommand()));
 	commandList.insert(std::make_pair("ping", new PingCommand()));
-
 	if (commandList.find(argv[1]) == commandList.end())
 		std::cerr << "Command " << argv[1] << " not found" << std::endl;
 	else
 		commandList[argv[1]]->execute();
-
 	return (0);
 }
