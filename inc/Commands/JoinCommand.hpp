725538e105b8ead1007330aeaa2fa901142f7fca
Theo ZERIBI <tzeribi@gmail.com>
2023-06-21 04:57:54 +0200

    start Join Command

--- inc/Commands/JoinCommand.hpp
+++ inc/Commands/JoinCommand.hpp
@@ -8,6 +8,9 @@ class JoinCommand : public Command
 {
 private:
 	std::string _name;
+
+	bool	_isJoinable(const std::string &channel);
+	bool	_parseArguments(std::string str);
 public:
 	JoinCommand();
 	virtual ~JoinCommand();
--- srcs/commands/JoinCommand.cpp
+++ srcs/commands/JoinCommand.cpp
@@ -11,7 +11,46 @@ JoinCommand::~JoinCommand(void)
 
 void JoinCommand::execute(int fds, Client *client)
 {
-	(void) fds;
-	(void) client;
-	std::cout << "[" << this->_name << "] : JoinCommand executed !" << std::endl;
+	std::string	cmd =	client->getCmds().front();
+	size_t		pos =	cmd.find(" ");
+	std::string	arg =	cmd.substr(cmd.find(" ") + 1);
+	Irc		&ircserv =	Irc::getInstance();
+
+	if (arg == cmd)
+		arg.clear();
+	std::cout << "POS = " << pos << std::endl;
+	std::cout << "[JoinCommand] CMD = " << cmd << std::endl << "ARG = " << arg << std::endl;
+	if (arg.empty() || !this->_parseArguments(arg))
+	{
+		ircserv.addReply(Reply(fds, ERR_NEEDMOREPARAMS(ircserv.getName(), client->getNick(), this->_name)));
+		return ;
+	}
+}
+
+bool JoinCommand::_parseArguments(std::string str)
+{
+	if (str.empty())
+		return (false);
+	if (str[0] == ' ')
+		str.erase(0, 1);
+	if (str.find(" ") != str.npos)
+		str = str.substr(0, str.find(" "));
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		if (isalpha(str[i]))
+			return (true);
+	}
+	return (false);
+}
+/*
+** #foo -> Public chan - Anyone can join.
+** &foo -> Private chan - Only invited users can join.
+*/
+bool JoinCommand::_isJoinable(const std::string &channel)
+{
+	if (channel.empty())
+		return (false);
+	if (channel[0] != '#' && channel[0] != '&')
+		return (false);
+	return (true);
 }
