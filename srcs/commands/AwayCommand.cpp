6867e3c4b4e654aaee90777d7010b4e5e9aba6da
Theo ZERIBI <tzeribi@gmail.com>
2023-06-21 06:18:11 +0200

    Change ChanVector to MapVector

--- srcs/commands/AwayCommand.cpp
+++ srcs/commands/AwayCommand.cpp
@@ -14,6 +14,4 @@ void AwayCommand::execute(int fds, Client *client)
 	(void) fds;
 	(void) client;
 	std::cout << "[" << this->_name << "] : AwayCommand executed !" << std::endl;
-	// std::vector<Reply> replyVector;
-	// _replyVector.push_back(Reply(client->getSockfd(), DEFINE_MACRO()));
 }
