76105b726135e03430d89e357d2ff77accde72cc
Theo ZERIBI <tzeribi@gmail.com>
2023-06-25 09:39:40 +0200

    ✨ feat(commands): add CapCommand and QuitCommand classes to handle CAP and QUIT IRC commands 🐛 fix(CapCommand.cpp): set correct _name value for CapCommand class 🐛 fix(QuitCommand.cpp): set correct _name value for QuitCommand class 🔨 refactor(Irc.cpp): add CapCommand and QuitCommand to commandList map in initCommand() method 🔨 refactor(client_input.cpp): remove CAP command handling from manageCommand() method in Irc class

---
+++ inc/Commands/CapCommand.hpp
@@ -0,0 +1,15 @@
+#ifndef CAPCOMMAND_HPP
+# define CAPCOMMAND_HPP
+# include "Command.hpp"
+
+class CapCommand : public Command
+{
+private:
+	std::string _name;
+public:
+	CapCommand();
+	virtual ~CapCommand();
+	void execute(int fds, Client *client);
+};
+
+#endif
---
+++ srcs/commands/CapCommand.cpp
@@ -0,0 +1,17 @@
+#include "CapCommand.hpp"
+
+CapCommand::CapCommand(void) :
+_name("AWAY")
+{
+	std::cout << "┣⮕ CapCommand." << std::endl;
+}
+
+CapCommand::~CapCommand(void)
+{}
+
+void CapCommand::execute(int fds, Client *client)
+{
+	(void) fds;
+	(void) client;
+	std::cout << "[" << this->_name << "] : CapCommand executed !" << std::endl;
+}
